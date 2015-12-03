6d1fd35f562f0cf2eeb0ff70231274cb923eb351
Peter <pzh@xiaoge.co>
2015-12-03 19:13:59 +0800

    基本网络请求

---
+++ SwiftWall/models/Article.swift
@@ -0,0 +1,27 @@
+//
+//  Article.swift
+//  Logon
+//
+//  Created by NashLegend on 15/11/20.
+//  Copyright © 2015年 NashLegend. All rights reserved.
+//
+
+import Foundation
+
+class Article {
+    var id = "";
+    var title = "";
+    var url = "";
+    var imageUrl = "";
+    var author = "";
+    var authorID = "";
+    var authorAvatarUrl = "";
+    var subjectName = "";
+    var subjectKey = "";
+    var date = "";
+    var commentNum = 0;
+    var likeNum = 0;
+    var summary = "";
+    var content = "";
+    var desc = false;
+}
